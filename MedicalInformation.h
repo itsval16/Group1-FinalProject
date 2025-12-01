eb5891b8a382dc3129c8baf93edb44531c50683b
Junjie Li <junjie.li@jjay.cuny.edu>
2025-11-30 22:43:54 -0500

    Update MedicalInformation.h

--- MedicalInformation.h
+++ MedicalInformation.h
@@ -2,4 +2,17 @@
 #ifndef MEDICALINFORMATION_H
 #define MEDICALINFORMATION_H
 
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class MedicalInformation {
+	private:
+		static fstream medicalInfo;
+	public:
+		void modifyInformation();
+		void getInformation();
+};
+
 #endif
