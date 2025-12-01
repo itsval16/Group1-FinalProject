c31ea4d8f5b6ca1be133970cc32b20d5f411571f
Junjie Li <junjie.li@jjay.cuny.edu>
2025-12-01 10:37:03 -0500

    Update MedicalInformation.cpp

--- MedicalInformation.cpp
+++ MedicalInformation.cpp
@@ -1,2 +1,9 @@
 // MedicalInformation.cpp
+
 #include "MedicalInformation.h"
+
+// string dob, string name, int age, string gender, string insuranceCompany, int insuranceNum, string diagnosis, vector<string> medication
+MedicalInformation::addInformation(){
+	medicalInfo.open("patientsInfo.txt", ios::app);
+	
+}
