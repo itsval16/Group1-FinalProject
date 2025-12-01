377368e9dcfb85f854ba44a30f4402f94a511573
Junjie Li <junjie.li@jjay.cuny.edu>
2025-12-01 12:36:13 -0500

    Update MedicalInformation.h

--- MedicalInformation.h
+++ MedicalInformation.h
@@ -12,9 +12,10 @@ class MedicalInformation {
 	private:
 		static fstream medicalInfo;
 	public:
-		void addInformation(const string& dob);
+		void addInformation(const string& info);
 		void modifyInformation(const string& dob, const string& name, const string& newInfo);
 		void getInformation(const string& dob, const string& name);
 };
 
 #endif
+
