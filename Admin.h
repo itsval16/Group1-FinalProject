2550b285707388d674fac69fd9b8cd90920fa6b9
Valeria <76661272+itsval16@users.noreply.github.com>
2025-12-02 10:46:45 -0500

    Implement Admin class for healthcare management
    Added Admin class with methods for managing doctors and patients.

--- Admin.h
+++ Admin.h
@@ -1,5 +1,28 @@
-// Admin.h
 #ifndef ADMIN_H
 #define ADMIN_H
+
+#include <iostream>
+#include <string>
+#include "MedicalInformation.h"
+
+using namespace std;
+
+class Admin {
+    private:
+        string adminName;
+        int adminID;
+
+        MedicalInformation medInfo;  
+        // Admin uses the same file system as patients & doctors
+
+    public:
+        Admin(string name = "Admin", int id = 0000);
+
+        void addDoctor(const string& doctorInfo);
+        void removeDoctor(const string& doctorName);
+        void updatePatient(const string& dob, const string& name, const string& newInfo);
+        void viewPatients(const string& dob, const string& name);
+        void displayInfo() const;
+};
 
 #endif
